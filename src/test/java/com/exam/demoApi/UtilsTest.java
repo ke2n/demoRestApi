49a7afdad278bf77581e65efcd4b375922e2d668
김윤성 <ke2n@naver.com>
2019-03-21 00:08:31 +0900

    테스트 코드 작성 & 테스트 코드에 따른 보완점 수정

--- src/test/java/com/exam/demoApi/UtilsTest.java
+++ src/test/java/com/exam/demoApi/UtilsTest.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +14,10 @@ import com.exam.demoApi.common.Utils;
 import com.exam.demoApi.domain.ResultInfo;
 import com.exam.demoApi.domain.SupportInfo;
 import com.nitorcreations.junit.runners.NestedRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author yunsung Kim
@@ -40,88 +43,88 @@ public class UtilsTest {
     public class CsvRead_메서드_테스트 {
 
         @Test
-        public void whenAllNullsValue() {
+        public void 모든_파라메터가_Null일때() {
             List resultList = Utils.csvRead(null, null);
-            Assert.assertEquals(resultList.size(), 0);
+            assertEquals(resultList.size(), 0);
         }
 
         @Test
-        public void whenClazzNullValue() {
+        public void Clazz파라메터가_Null일때() {
             List resultList = Utils.csvRead(null, fin);
-            Assert.assertEquals(resultList.size(), 0);
+            assertEquals(resultList.size(), 0);
         }
 
         @Test
-        public void whenStreamNullValue() {
+        public void Stream이_Null일때() {
             List resultList = Utils.csvRead(ResultInfo.class, null);
-            Assert.assertEquals(resultList.size(), 0);
+            assertEquals(resultList.size(), 0);
         }
 
         @Test
         public void whenStreamIOExceptionValue() {
             List resultList = Utils.csvRead(SupportInfo.class, null);
-            Assert.assertEquals(resultList.size(), 0);
+            assertEquals(resultList.size(), 0);
         }
 
         @Test
         public void UTF8형식의_파일이_아닌경우() {
             // TODO: 해당 case 작성 필요
             List resultList = Utils.csvRead(ResultInfo.class, finNoUtf8);
-            Assert.assertFalse(CollectionUtils.isEmpty(resultList));
+            assertTrue(CollectionUtils.isEmpty(resultList));
         }
 
         @Test
-        public void whenSuccessValue() {
+        public void 정상케이스일때() {
             List resultList = Utils.csvRead(ResultInfo.class, fin);
-            Assert.assertFalse(CollectionUtils.isEmpty(resultList));
+            assertFalse(CollectionUtils.isEmpty(resultList));
         }
     }
 
     public class convertRate_메서드_테스트 {
 
         @Test
-        public void whenNullValue() {
+        public void 파라메터가_Null일때() {
             double result = Utils.convertAvgRate(null);
-            Assert.assertEquals(result, Double.NaN, 0.0);
+            assertEquals(result, Double.NaN, 0.0);
 
             double result2 = Utils.convertMinRate(null);
-            Assert.assertEquals(result2, Double.NaN, 0.0);
+            assertEquals(result2, Double.NaN, 0.0);
         }
 
         @Test
-        public void whenEmptyStringValue() {
+        public void 빈스트링값_일때() {
             double result = Utils.convertAvgRate("");
-            Assert.assertEquals(result, Double.NaN, 0.0);
+            assertEquals(result, Double.NaN, 0.0);
 
             double result2 = Utils.convertMinRate("");
-            Assert.assertEquals(result2, Double.NaN, 0.0);
+            assertEquals(result2, Double.NaN, 0.0);
         }
 
         @Test
-        public void whenNotIncludeNumberStringValue() {
+        public void 스트링에_숫자값이_포함되지_않았을때() {
             double result = Utils.convertAvgRate("ABCDEFG");
-            Assert.assertEquals(result, Double.NaN, 0.0);
+            assertEquals(result, Double.NaN, 0.0);
 
             double result2 = Utils.convertMinRate("ABCDEFG");
-            Assert.assertEquals(result2, Double.NaN, 0.0);
+            assertEquals(result2, Double.NaN, 0.0);
         }
 
         @Test
-        public void whenIncludeSingleNumberStringValue() {
+        public void 스트링에_숫자값이_한번만_출현하였을때() {
             double result = Utils.convertAvgRate("0.5% 입니다.");
-            Assert.assertEquals(result, 0.5, 0.0);
+            assertEquals(result, 0.5, 0.0);
 
             double result2 = Utils.convertMinRate("0.5% 입니다.");
-            Assert.assertEquals(result2, 0.5, 0.0);
+            assertEquals(result2, 0.5, 0.0);
         }
 
         @Test
-        public void whenIncludeMultipleNumberStringValue() {
+        public void 스트링에_숫자값이_두번이상_출현하였을때() {
             double result = Utils.convertAvgRate("40.221% 부터 0.5% ~ 20.0");
-            Assert.assertEquals(result, (40.221 + 0.5 + 20.0) / 3, 0.0);
+            assertEquals(result, (40.221 + 0.5 + 20.0) / 3, 0.0);
 
             double result2 = Utils.convertMinRate("40.221% 부터 0.5% ~ 20.0");
-            Assert.assertEquals(result2, 0.5, 0.0);
+            assertEquals(result2, 0.5, 0.0);
         }
 
 
@@ -130,39 +133,39 @@ public class UtilsTest {
     public class limitToNumberConverter_메서드_테스트 {
 
         @Test
-        public void whenNullValue() {
+        public void 파라메터가_Null일때() {
             long result = Utils.limitToNumberConverter(null);
-            Assert.assertEquals(result, 0);
+            assertEquals(result, 0);
         }
 
         @Test
-        public void whenEmptyStringValue() {
+        public void 빈스트링값_일때() {
             long result = Utils.limitToNumberConverter("");
-            Assert.assertEquals(result, 0);
+            assertEquals(result, 0);
         }
 
         @Test
-        public void whenNotIncludeNumberStringValue() {
+        public void 스트링에_숫자값이_포함되지_않았을때() {
             long result = Utils.limitToNumberConverter("ABCDEFG");
-            Assert.assertEquals(result, 0);
+            assertEquals(result, 0);
         }
 
         @Test
-        public void whenIncludeMultipleNumberStringValue() {
+        public void 스트링에_숫자값이_두번이상_출현하였을때() {
             long result = Utils.limitToNumberConverter("12ABCDEFG11");
-            Assert.assertEquals(result, 12);
+            assertEquals(result, 12);
         }
 
         @Test
-        public void whenIncludeNumberAndPostfixStringValue() {
+        public void 스트링에_숫자값과_금액이_문자로_출현하였을때() {
             long result = Utils.limitToNumberConverter("12억원");
-            Assert.assertEquals(result, 1200000000);
+            assertEquals(result, 1200000000);
         }
 
         @Test
-        public void whenIncludeNumberAndPostfixStringValue2() {
+        public void 스트링에_숫자값과_금액이_문자로_출현하였을때_2() {
             long result = Utils.limitToNumberConverter("100백만원");
-            Assert.assertEquals(result, 100000000);
+            assertEquals(result, 100000000);
         }
 
     }
